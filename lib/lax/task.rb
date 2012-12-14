fcdc6e5cc5c0440ab31e70481bd5c5add604212e
feivel <feivel@sdf.org>
2012-12-14 00:37:24 -0500

    task changes

--- lib/lax/task.rb
+++ lib/lax/task.rb
@@ -1,34 +1,36 @@
 require 'rake'
 module Lax
-  class Task
-    include Rake::DSL
-    def initialize(opts = {})
-      dir = opts.delete(:dir) || :test
-      make_tasks dir, {
-        start:  Hook::StartTime,
-        after:  Hook::PassFail,
-        finish: Hook::StopTime + Hook::Summary + Hook::Failures
-      }.merge(opts)
-    end
-
-    private
-    def make_tasks(dir, opts)
-      namespace dir do
-        desc "[Lax] load all test files"
-        task load: make_groups(dir)
-        desc "[Lax] run all loaded tests"
-        task(:run) { Lax.test! Lax.cases, opts }
+  module Task
+    class << self
+      include Rake::DSL
+      def new(opts = {})
+        dir = opts.delete(:dir) || :test
+        make_tasks dir, {
+          start:  Hook::StartTime,
+          after:  Hook::PassFail,
+          finish: Hook::StopTime + Hook::Summary + Hook::Failures
+        }.merge(opts)
       end
-      desc "[Lax] load and run all tests"
-      task dir => ["#{dir}:load","#{dir}:run"]
-    end
 
-    def make_groups(dir)
-      FileList["#{dir}/**/*"].select {|f| File.directory? f}.map do |group|
-        name = group.sub(/^#{dir}\//,'').gsub(/\//,?:)
-        desc "[Lax] load files in #{group}"
-        task(name) { Dir["#{group}/*.rb"].each {|file| load file} }
-        [dir,name]*?:
+      private
+      def make_tasks(dir, opts)
+        namespace dir do
+          desc "[Lax] load all test files"
+          task load: make_groups(dir)
+          desc "[Lax] run all loaded tests"
+          task(:run) { Lax.test! Lax.cases, opts }
+        end
+        desc "[Lax] load and run all tests"
+        task dir => ["#{dir}:load","#{dir}:run"]
+      end
+
+      def make_groups(dir)
+        FileList["#{dir}/**/*"].select {|f| File.directory? f}.map do |group|
+          name = group.sub(/^#{dir}\//,'').gsub(/\//,?:)
+          desc "[Lax] load files in #{group}"
+          task(name) { Dir["#{group}/*.rb"].each {|file| load file} }
+          [dir,name]*?:
+        end
       end
     end
   end
