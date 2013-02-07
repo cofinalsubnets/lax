620b63708e8ceeaaf905bed4644cfd9e79d89c7b
feivel <feivel@sdf.org>
2013-02-07 04:13:26 -0500

    unbreak tests & rakefile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require 'rspec'
 require 'lax'
-require 'pry'
-require_relative 'support/matchers'
-require_relative 'support/targets'
+#require 'pry'
 
 RSpec.configure do |config|
   config.color_enabled = true
